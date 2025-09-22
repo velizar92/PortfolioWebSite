4efa4a67f05adb6aadda7c5a2793941fa5332002
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-09-22 15:49:19 +0300

    Change ApplicationDbContext to inherit from IdentityDbContext.

--- WebPortfolio/Portfolio.Data/Data/ApplicationDbContext.cs
+++ WebPortfolio/Portfolio.Data/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Portfolio.Domain.Entities;
 
 namespace Portfolio.Infrastructure.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
