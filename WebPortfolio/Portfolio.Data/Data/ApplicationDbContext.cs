a48c3073387f8b7aa1b30cd324445c82b758732a
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-09-22 15:41:14 +0300

    Create ApplicationDbContext.cs

---
+++ WebPortfolio/Portfolio.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Domain.Entities;
+
+namespace Portfolio.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ContactMessage> ContactMessages { get; set; }
+        public DbSet<ProjectCategory> ProjectCategories { get; set; }
+    }
+}
