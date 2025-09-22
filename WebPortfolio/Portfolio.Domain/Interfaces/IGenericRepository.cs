de1f37e3a54e7d4d00c6f71d06b1ffdebdb9e2cf
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-09-22 13:53:18 +0300

    Add IGenericRepository.cs

---
+++ WebPortfolio/Portfolio.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Portfolio.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+        Task<bool> SaveChangesAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    }
+}
