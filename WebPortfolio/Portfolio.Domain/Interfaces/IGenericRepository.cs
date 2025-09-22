575647177eabfc3bb75199cf4bd808ecf43f77d0
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-09-22 14:13:11 +0300

    Update IGenericRepository interface API

--- WebPortfolio/Portfolio.Domain/Interfaces/IGenericRepository.cs
+++ WebPortfolio/Portfolio.Domain/Interfaces/IGenericRepository.cs
@@ -5,11 +5,11 @@ namespace Portfolio.Domain.Interfaces
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(int id);
         Task AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(int id);
+        void Update(T entity);
+        void Delete(T entity);
         Task<bool> SaveChangesAsync();
-        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);      
     }
 }
