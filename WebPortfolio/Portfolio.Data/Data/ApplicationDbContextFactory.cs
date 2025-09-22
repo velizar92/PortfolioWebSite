a65984d5d4508b7db1fac228a53bdec8895c048a
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-09-22 20:16:24 +0300

    Add ApplicationDbContextFactory for creating of DbContext obj in design time.

---
+++ WebPortfolio/Portfolio.Data/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Infrastructure.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Determine path to settings file
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+            var jsonFilePath = Path.Combine(Directory.GetCurrentDirectory(), $"appsettings.{environment}.json");
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(jsonFilePath, optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
